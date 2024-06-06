0be457667bb0fcd7898c7dc72221d3365fec6a71
shut-sh <ashutosh@ashutoshirole.com>
2024-06-06 16:53:53 +0200

    fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mylittlesecret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, debug=True, cors_allowed_origins='*', async_mode='eventlet')
 
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 pi_ugv = UGV()
