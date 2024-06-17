94f27260741156657fdaab6c614393f729bb67fb
shut-sh <ashutosh@ashutoshirole.com>
2024-06-17 12:29:33 +0200

    fixes

--- ugv.py
+++ ugv.py
@@ -6,36 +6,32 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Set which GPIO pins the drive outputs are connected to
-DRIVE_1 = 4
-DRIVE_2 = 18
-DRIVE_3 = 8
-DRIVE_4 = 7
+# DRIVE_1 = 4
+# DRIVE_2 = 18
+# DRIVE_3 = 8
+# DRIVE_4 = 7
 
 LEFT_MOTOR = 14 #18
 RIGHT_MOTOR = 23 #8
 
-PAN_NO = 1
-TILT_NO = 0
-ANGLE_STEP = 10
+PAN_NO = 0
+TILT_NO = 1
+ANGLE_STEP = 5
 
 
 class UGV(object):
     
     def __init__(self):
-        GPIO.setup(DRIVE_1, GPIO.OUT)
-        GPIO.setup(DRIVE_2, GPIO.OUT)
-        GPIO.setup(DRIVE_3, GPIO.OUT)
-        GPIO.setup(DRIVE_4, GPIO.OUT)
+        GPIO.setup(LEFT_MOTOR, GPIO.OUT)
+        GPIO.setup(RIGHT_MOTOR, GPIO.OUT)
         self.pwm = PCA9685()
         self.pwm.setPWMFreq(50)
         self.pan_angle = 10
         self.tilt_angle = 10
 
     def close(self):
-        GPIO.output(DRIVE_1, GPIO.LOW)
-        GPIO.output(DRIVE_2, GPIO.LOW)
-        GPIO.output(DRIVE_3, GPIO.LOW)
-        GPIO.output(DRIVE_4, GPIO.LOW)
+        GPIO.output(LEFT_MOTOR, GPIO.LOW)
+        GPIO.output(RIGHT_MOTOR, GPIO.LOW)
         GPIO.cleanup()
         self.pwm.exit_PCA9685()
         self.set_pan_angle(90)
@@ -80,17 +76,21 @@ class UGV(object):
             time.sleep(0.01)
     
     def camera_up(self):
+        print(f"tilt_angle: {self.tilt_angle}")
         new_angle = self.tilt_angle + ANGLE_STEP
         self.set_tilt_angle(new_angle)
     
     def camera_down(self):
+        print(f"tilt_angle: {self.tilt_angle}")
         new_angle = self.tilt_angle - ANGLE_STEP
         self.set_tilt_angle(new_angle)
     
     def camera_left(self):
+        print(f"pan_angle: {self.pan_angle}")
         new_angle = self.pan_angle + ANGLE_STEP
         self.set_pan_angle(new_angle)
     
     def camera_right(self):
+        print(f"pan_angle: {self.pan_angle}")
         new_angle = self.pan_angle - ANGLE_STEP
         self.set_pan_angle(new_angle)
